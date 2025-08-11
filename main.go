31e41b63fba89690cfa4dcb73977d139cae1a4d9
Ellie <github@lly.email>
2025-08-10 23:49:22 -0500

    nit: one filing for now

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello, world!")
+func cleanInput(text string) string {
+	rawExpression := strings.Fields(text)
+	cleanExpression := strings.Join(rawExpression, "")
+	return cleanExpression
+}
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Enter expression >> ")
+		reader.Scan()
+
+		//exp := reader.Text()
+	}
 }
